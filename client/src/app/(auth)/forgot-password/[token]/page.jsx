81a287ab770b3bb24169c19f29c181177f478f24
AfaqShahidKhan <afaqk3203@gmail.com>
2025-02-11 12:48:57 +0500

    modify seo

--- client/src/app/(auth)/forgot-password/[token]/page.jsx
+++ client/src/app/(auth)/forgot-password/[token]/page.jsx
@@ -1,16 +1,11 @@
-import Register from "@/components/auth/Register";
 import React from "react";
 export const metadata = {
-  title: "Register",
+  title: "Forgot-Password",
   description:
     "Join Dialogue Room, a platform designed to foster meaningful conversations. Connect with like-minded individuals, learn new skills, and grow through interactive discussions. Whether you're looking to exchange ideas, improve your communication, or just have a good chat, Dialogue Room is the place to be.",
 };
 const page = () => {
-  return (
-    <div>
-      <Register />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default page;
--- client/src/app/(auth)/login/page.jsx
+++ client/src/app/(auth)/login/page.jsx
@@ -1,6 +1,10 @@
 import Login from '@/components/auth/Login'
 import React from 'react'
-
+export const metadata = {
+  title: "Login",
+  description:
+    "Join Dialogue Room, a platform designed to foster meaningful conversations. Connect with like-minded individuals, learn new skills, and grow through interactive discussions. Whether you're looking to exchange ideas, improve your communication, or just have a good chat, Dialogue Room is the place to be.",
+};
 const page = () => {
   return (
     <div>
